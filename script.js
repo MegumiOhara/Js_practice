75102b3f1c95949381ba11f688c279a35e393d76
MegumiOhara <156937802+MegumiOhara@users.noreply.github.com>
2024-03-28 14:15:59 +0100

    calculated

---
+++ script.js
@@ -0,0 +1,15 @@
+const triangleOne = (10*13)/2;
+const triangleTwo = (16.5*20.4)/2;
+console.log(triangleOne, triangleTwo);
+
+const triangleThree = (16.5*20.3)/2;
+const triangleFour = (20.3*16.5)/2; 
+console.log(triangleThree, triangleFour)
+
+const triangleFive = (7.8*5.6)/2;
+const triangleSix = (9.3*12.4)/2;
+console.log(triangleFive, triangleSix);
+
+console.log(triangleOne > triangleTwo);
+console.log(triangleThree > triangleFour);
+console.log(triangleFive > triangleSix);
